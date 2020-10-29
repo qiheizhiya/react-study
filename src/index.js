d87c8af6d18e1a2166b3510c5859c8b110a08e26
隆杰 <953136447@qq.com>
2020-10-29 14:25:42 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './Test'
+import Test from './components/Test'
 ReactDOM.render(<Test>32131221</Test> , document.getElementById("root"))
