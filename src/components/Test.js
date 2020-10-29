177efb9d0a4a217901e1d851d83ae92208cf5616
隆杰 <953136447@qq.com>
2020-10-29 14:38:12 +0800

    完成

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react'
-import Modal from './Modal'
+import ThreeLayout from './ThreeLayout'
+
 export default class Test extends Component {
-    state = {
-        showModal: false
-    }
-    showModal = () => {
-        this.setState({
-            showModal: true
-        })
-    }
-    hideModal = () => {
-        this.setState({
-            showModal: false
-        })
-    }
     render() {
         return (
             <div>
-                {
-                    this.state.showModal ? <Modal onClose={this.hideModal}>
-                        <h1>eqwwqeqeqwqqqwqw</h1>
-                        <button onClick={this.hideModal}>关闭蒙层</button>
-                    </Modal> : null
-                }
-                <p>
-                    <button onClick={this.showModal}>显示蒙层</button>
-                </p>
+                <ThreeLayout leftContent={ <h2>左边的还行</h2>} rightContent={ <h2>右边的也是</h2> }>
+                    <h1>中间的牛比</h1>
+                </ThreeLayout>
             </div>
         )
     }
