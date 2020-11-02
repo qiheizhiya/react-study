86deeed33f4af412bfa4cb131d08c8466486da7a
qiheizhiya <953136447@qq.com>
2020-11-02 17:55:45 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react'
-import RadioGroup from './components/RadioGroup'
-import { getAllStud } from './fetch'
-export default class App extends Component {
-  state = {
-    datas: [
-      { value: 'football', text: '足球' },
-      { value: 'basketball', text: '篮球' }
-    ],
-    chooseDatas: ''
-  }
-
-  async componentDidMount () {
-    const allStu = await getAllStud()
-    this.setState({
-      datas: allStu.data.findByPage.map(it => ({ value: it.id.toString(), text: it.name }))
-    })
-  }
-
-  render() {
+import React from 'react'
+import ValidationComp, { A } from "./ValidataionComp"
+import Comp from "./Comp"
+export default function App() {
     return (
-      <div>
-        <RadioGroup name='loves' {...this.state} onChange={(data) => {
-          this.setState({ chooseDatas: data })
-        }} />
-      </div>
+        <div>
+            <ValidationComp
+                a={2}
+                d={<Comp />}
+                e={<Comp />}
+                F={Comp}
+                g={new A()}
+                sex="男"
+                h={[2, 3]}
+                i={{
+                    a: 2
+                }}
+                j={{
+                    a: 3,
+                    name: "abc",
+                    age: 233,
+                    address: {
+                        province: "asdfa",
+                        city: "adsfasdf"
+                    }
+                }}
+                k={[{name:"asdf", age:33}]}
+                m={23}
+                score={33}
+            />
+        </div>
     )
-  }
 }
