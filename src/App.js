cc9f62d7d98bc623bd89ea789128bdb9236edf98
qiheizhiya <953136447@qq.com>
2020-11-07 14:21:01 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import Test from './components/CheckboxGroup/Test'
+import { A, B } from './components/Comps'
+import withLogin from './HOC/withLogin' 
+
+const ALog = withLogin(A)
+const BLog = withLogin(B)
+
 export default function App() {
     return (
         <div>
-          <Test />
+          <ALog a={1} isLogin />
+          <BLog b={2} isLogin />
         </div>
     )
 }
