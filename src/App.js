ce695e0d94a206dc4ba553e6ba69fdeada606d1b
隆杰 <953136447@qq.com>
2020-11-14 11:06:34 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import OldContext from './OldContext'
-
+import Test from "./components/Test"
 
 export default function App() {
-
     return (
-        <div>
-          <OldContext />
-        </div>
+        <Test />
     )
 }
