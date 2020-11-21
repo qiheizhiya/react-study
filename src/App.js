ecaf66a93bdcfae719054ec3403f11e0d8799fa3
qiheizhiya <953136447@qq.com>
2020-11-21 22:14:52 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+let n = 1;
+
+function func1() {
+    console.log("odd 副作用函数")
+    return () => {
+        console.log("odd 清理函数")
+    }
+}
+
+function func2() {
+    console.log("even 副作用函数")
+    return () => {
+        console.log("even 清理函数")
+    }
+}
 
 export default function App() {
-    console.log('我渲染了')
-    const [n, setN] = useState(0)
+    const [, forceUpdate] = useState({})
+    useEffect(n % 2 === 0 ? func2 : func1);
+    n++;
     return (
         <div>
             <button onClick={() => {
-                setN(n - 1)
-            }}>-</button>
-            <h1>{n}</h1>
-            <button onClick={() => {
-                setN(n + 1)
-            }}>+</button>
-            <button onClick={() => {
-                setN(n)
-            }}>-----------</button>
+                forceUpdate({});
+            }}>强制刷新</button>
         </div>
     )
 }
