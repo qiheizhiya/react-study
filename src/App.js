69f83b81ae6e057298efc608cb7711568cb081c4
qiheizhiya <953136447@qq.com>
2020-11-22 14:50:05 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { useState } from 'react'
-// import useAllStudent from './myHooks/useAllStudent'
-import usePageStudent from './myHooks/usePageStudent'
-import Pager from './components/Pager';
-function Test() {
-    const [page, setPage] = useState(1)
-    const { data, total } = usePageStudent(page)
-    const [limit] = useState(10)
-    const list = data.map(it => <li key={it.id}>{it.name}</li>)
+import React, { useContext } from 'react'
 
+const ctx = React.createContext()
+
+function Test () {
+    const value = useContext(ctx)
     return (
-        <>
-            <p>数据总量：{total}</p>
-            <ul>
-                {list}
-            </ul>
-            <Pager current={page} limit={limit} total={total} onPageChange={newPage => setPage(newPage)} />
-        </>
+        <div>我的值是：{value}</div>
     )
 }
 
 export default function App() {
     return (
-        <div>
+        <ctx.Provider value="abc">
             <Test />
-        </div>
+        </ctx.Provider>
     )
 }
