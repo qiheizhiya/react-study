b1775da70791bac3dcaee469be44778eea61c1d2
qiheizhiya <953136447@qq.com>
2020-11-22 21:07:51 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useMemo } from 'react'
 
-class Test extends React.PureComponent {
-    render () {
-        console.log("Test Render");
-        return (
-            <div>
-                <h1>{this.props.text}</h1>
-                <button onClick={this.props.onClick}>点我啊</button>
-            </div>
-        )
-    }
-    
+function Item(props) {
+    return <li>{props.value}</li>
 }
 
 export default function App() {
-    console.log('App render');
-    const [txt, setTxt] = useState(123)
+    const [range] = useState({ min: 1, max: 1000 })
     const [n, setN] = useState(0)
+    const list = useMemo(() => {
+        const list = []
+        for (let i = range.min; i <= range.max; i++) {
+            list.push(<Item key={i} value={i}></Item>)
+        }
+        return list
+    }, [range])
 
-    const handleClick = useCallback(() => {
-        setTxt(Math.random())
-    }, [])
-
+    
     return (
         <div>
-            <Test text={txt} onClick={handleClick} />
-            <input type="number" value={n} onChange={e => {
-                setN(e.target.value)
-            }} />
+            {list}
+            <p>
+                <input type="number" value={n} onChange={e => {
+                    setN(e.target.value)
+                }} />
+            </p>
         </div>
     )
 }
