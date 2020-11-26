45d3a3c60d5d6c1510e06effe4d7c9284f610f08
qiheizhiya <953136447@qq.com>
2020-11-26 23:27:45 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { HashRouter, Route } from 'react-router-dom'
+
+function A () {
+    return <h1>组件1</h1>
+}
+
+function B () {
+    return <h1>组件2</h1>
+}
+
+function C () {
+    return <h1>组件3</h1>
+}
 
 export default function App() {
     return (
-        <div>
-            1
-        </div>
+        <HashRouter>
+            <Route path='/a' exact component={A}></Route>
+            <Route path='/a/b' exact component={B}></Route>
+            <Route path='/c' exact component={C}></Route>
+        </HashRouter>
     )
 }
