b4bc4d546597d7825f780b8a687c61242f7f7b3f
qiheizhiya <953136447@qq.com>
2020-11-28 18:48:42 +0800

    完成

--- src/App copy.js
+++ src/App copy.js
@@ -1,34 +1,41 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-// import Link from "./Link"
 
-function PageA() {
-    return <h1>A页</h1>
-}
-
-function PageB() {
-    return <h1>B页</h1>
-}
-
-function NavBar() {
+function User({match}) {
     return <div>
-        <Link innerRef={node=>{
-            console.log(node)
-        }} to="/a" style={{ marginRight: 20 }}>去a页</Link>
-        <Link replace to={{
-            pathname: "/b",
-            hash: "#abc",
-            search: "?a=1&b=2"
-        }}>去b页</Link>
+        <h1>User组件固定的区域</h1>
+        <p>
+            <Link to={`${match.url}/update`}>用户信息</Link>
+            <Link to={`${match.url}/pay`}>充值</Link>
+        </p>
+        <div style={{
+            width: 500,
+            height: 500,
+            background: "lightblue",
+            border: "2px solid",
+            margin: "0 auto"
+        }}>
+            {/* User组件变化的区域：根据地址的不同发生变化 */}
+            <Route path={`${match.url}/update`} component={UserUpdate} />
+            <Route path={`${match.url}/pay`} component={UserPay} />
+        </div>
+
     </div>
+}
+
+function UserUpdate() {
+    return <h1>修改用户信息</h1>
+}
+
+function UserPay() {
+    return <h1>用户充值</h1>
 }
 
 export default function App() {
     return (
         <Router>
-            <NavBar />
-            <Route path="/a" component={PageA} />
-            <Route path="/b" component={PageB} />
+            <Route path="/u" component={User} />
+            {/* 其他组件 */}
         </Router>
     )
 }
