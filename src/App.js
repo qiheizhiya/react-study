e96bef52b1649af07025892a3bebc5be4dc4835c
隆杰 <953136447@qq.com>
2020-12-04 16:38:54 +0800

    完成

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom"
-import Page1 from './Page1'
-import Page2 from './Page2'
 
 export default function App() {
     return (
-        <Router getUserConfirmation={(msg, callback) => {
-            console.log(msg)
-            callback(window.confirm(msg));
-        }}>
-            <div className="nav">
-                <NavLink to="/page1">页面1</NavLink>
-                <NavLink to="/page2">页面2</NavLink>
-            </div>
-            <div className="container">
-                <Route path="/page1" component={Page1} />
-                <Route path="/page2" component={Page2} />
-            </div>
-        </Router>
+        <div>
+            App
+        </div>
     )
 }
