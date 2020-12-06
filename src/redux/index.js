751df8310a68b50bccf6359b286b414706cacdb6
qiheizhiya <953136447@qq.com>
2020-12-06 16:55:41 +0800

    完成

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,14 +1,22 @@
-import reducer from './reducer'
-import { createStore } from 'redux'
-// import { createSetLoginUserAction } from './action/loginUserAction'
-import { createAddUserAction } from './action/usersAction'
-const store = createStore(reducer)
+import { createStore } from "redux";
+import reducer from "./reducer"
+import { createAddUserAction, createDeleteUserAction } from "./action/usersAction"
 
 
-console.log(store.getState())
-store.dispatch(createAddUserAction({id: 3, name: '呱呱3号'}))
-console.log(store.getState())
-store.dispatch(createAddUserAction({id: 4, name: '呱呱4号'}))
-console.log(store.getState())
-store.dispatch(createAddUserAction({id: 5, name: '呱呱5号'}))
-console.log(store.getState())
+const store = createStore(reducer);
+
+console.log(store)
+
+const unListen = store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch(createAddUserAction({
+    id: 3,
+    name: "abc",
+    age: 10
+}));
+
+unListen(); //取消监听
+
+store.dispatch(createDeleteUserAction(3));
