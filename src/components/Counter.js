39486ec2e32871c84b5cd12c1b2a2fb30f9001dc
qiheizhiya <953136447@qq.com>
2021-01-07 23:14:13 +0800

    完成

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { connect } from 'umi'
+import styles from './counter.less'
+console.log(styles);
 function Counter(props) {
     console.log(props);
     return (
-        <div>
+        <div className={styles.counter}>
             <h1>{props.number}</h1>
             <p>
                 <button onClick={props.onDecrease}>减</button>
