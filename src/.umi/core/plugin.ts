47717f032776acf49de34095c0b9a087f5d23291
qiheizhiya <953136447@qq.com>
2021-01-10 18:21:54 +0800

    完成

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'E:/我的项目相关/15.react/react-study/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','dva','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
